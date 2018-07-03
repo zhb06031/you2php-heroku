ddd0379b00fd9659724f3ed1f0abbb20b610031c
zhb06031 <36952183+zhb06031@users.noreply.github.com>
2018-07-03 10:39:38 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyDhlObYG8898MbLgkKXz-N-jCd9FKGWDZw');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', '内马尔霸气回应被踩：踩我没用，他们还是出局了');//网站名字
 define('TITLENAME', '内马尔霸气回应被踩：踩我没用，他们还是出局了');//网站名字
 define('EN2DEKEY', 'Q62LBamog9NGFCdBLao2');//一个随机字符串，加解密KEY
