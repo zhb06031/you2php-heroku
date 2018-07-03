ee54dd2a75ad464fa9e77a954040b5421872f530
zhb06031 <36952183+zhb06031@users.noreply.github.com>
2018-07-03 10:39:24 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
+define('APIKEY', 'AIzaSyDhlObYG8898MbLgkKXz-N-jCd9FKGWDZw');//这里改成您的api key
 define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('SITE_NAME', '内马尔霸气回应被踩：踩我没用，他们还是出局了');//网站名字
+define('TITLENAME', '内马尔霸气回应被踩：踩我没用，他们还是出局了');//网站名字
+define('EN2DEKEY', 'Q62LBamog9NGFCdBLao2');//一个随机字符串，加解密KEY
+define('EMAIL', 'zhb060317@gmail.com');//邮箱
 ?>
